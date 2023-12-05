c699bf84c7117ad6f03f93906460e1c995af44af
Ankit Rane <91012632+ankit-rane@users.noreply.github.com>
2023-12-06 00:47:02 +0530

    Create 1. Average Marks.py

---
+++ 1. Average Marks.py
@@ -0,0 +1,10 @@
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+def averageMarks(firstLetterOfName, m1, m2, m3):
+
+    average = floor((m1 + m2 + m3) / 3)
+    return (firstLetterOfName, average)
+    pass
