c2b5bc5bac388924ceac367ce84c67283f1bd1d5
Ankit Rane <91012632+ankit-rane@users.noreply.github.com>
2023-12-06 01:20:12 +0530

    Create 7. Bucket Sort.java

---
+++ 7. Bucket Sort.java
@@ -0,0 +1,10 @@
+import java.util.* ;
+import java.io.*; 
+
+
+public class Solution {
+    static double[] sortArray(int n, double arr[]) {
+        Arrays.sort(arr);
+        return arr;
+    }
+}
